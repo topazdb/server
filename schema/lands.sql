eaa3626663b4eed866e6606f1f55208d14220d8d
Talen Fisher <tqfisher@iastate.edu>
2018-10-31 09:58:27 -0500

    add lands table schema

---
+++ schema/lands.sql
@@ -0,0 +1,14 @@
+drop table if exists lands;
+create table lands (
+
+    /** Columns **/
+    `id` bigint(20) unsigned primary key auto_increment,
+    `scanId` bigint(20) unsigned not null,
+    `path` text default null,
+
+    /** Constraints **/
+    constraint `lands_scanId_fk`
+        foreign key (`scanId`) references `scans` (`id`)
+        on delete cascade;
+
+) engine=InnoDB default charset=utf8;
