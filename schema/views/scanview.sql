d90f1a0acbe4030248290f3c160b8efa18e9f65f
Talen Fisher <tqfisher@iastate.edu>
2018-11-05 11:16:42 -0600

    add setName to scanView
    Signed-off-by: Talen Fisher <tqfisher@iastate.edu>

--- schema/views/scanview.sql
+++ schema/views/scanview.sql
@@ -4,13 +4,15 @@ create sql security invoker view scanView as
 select
     `scans`.*,
     `authors`.`name` as `authorName`,
+    `sets`.`name` as `setName`,
     count(distinct `scans`.`bulletNo`) as `bulletCount`,
     count(distinct `scans`.`barrelNo`) as `barrelCount`,
-    count(`lands`.`id`) as `landCount`
+    count(`lands`.`id`) as `landCount`,
 
 from `scans`
     left join `authors` on `scans`.`authorId`=`authors`.`id`
     left join `lands` on `lands`.`scanId`=`scans`.`id`
+    left join `sets` on `scans`.`setId`=`sets`.`id`
 
 group by
     `scans`.`id`;
