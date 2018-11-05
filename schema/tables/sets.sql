243de1c250d59e682205ddd0b8d0857b4620185a
Talen Fisher <tqfisher@iastate.edu>
2018-11-05 11:14:23 -0600

    add sets table
    Signed-off-by: Talen Fisher <tqfisher@iastate.edu>

---
+++ schema/tables/sets.sql
@@ -0,0 +1,6 @@
+drop table if exists sets;
+create table sets (
+    `id` bigint(20) unsigned primary key auto_increment,
+    `name` varchar(300) not null,
+    `creationDate` timestamp default current_timestamp
+) engine=InnoDB default charset=utf8 auto_increment=1;
