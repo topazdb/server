c7a91ba7b1cbe3108d084dfcaef6d13fb5e3c4e0
Talen Fisher <tqfisher@iastate.edu>
2019-02-12 13:41:58 -0600

    set calibrationDate default to current_timestamp

--- schema/tables/instruments.sql
+++ schema/tables/instruments.sql
@@ -2,5 +2,5 @@ create table if not exists instruments (
     `id` bigint(20) unsigned primary key auto_increment,
     `instrumentTypeId` bigint(20) unsigned not null,
     `serialNo` varchar(300) null,
-    `calibrationDate` timestamp default '0000-00-00 00:00:00'
+    `calibrationDate` timestamp default current_timestamp
 ) engine=InnoDB default charset=utf8 auto_increment=1;
